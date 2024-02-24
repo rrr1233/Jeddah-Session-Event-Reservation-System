268d8eb0c035a17d098cf8b18d550b83eb935380
rrr1233 <97314696+rrr1233@users.noreply.github.com>
2024-02-24 03:51:16 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Final_Project_OOP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        
+        new Wendow1();
